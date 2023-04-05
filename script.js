6ab2e41de256c92ab65fcc65e1bfd6ddb82cad7e
Ben Simpson <simpson.ben1@outlook.com>
2023-04-05 12:01:36 +0100

    Made changes to the buttons and added the function for game

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const rock = 0;
-const paper = 1; 
-const scissors = 2;
-
-
-function getComputerChoice () {
-
-    let computerChoice = Math.floor(Math.random() * 3); 
-
-    if (computerChoice === rock) {
-        console.log("rock");
-    } else if (computerChoice === paper) {
-        console.log("Paper");
-    } else if (computerChoice === scissors) {
-        console.log("scissors");
-    } else {
-        return "No choice was made"
+function getComputerChoice(player) {
+    // Create Array of choices
+    const choices = ["rock", "paper", "scissors"];
+    
+    // Generate between 0 and 2, and use it to select choice
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+  
+    // Check for a draw
+    if (player === computerChoice) {
+      console.log("Draw");
+    } 
+    // Check player win
+    else if (
+      (player === "rock" && computerChoice === "scissors") || // Rock >scissors
+      (player === "paper" && computerChoice === "rock") || // Paper > rock
+      (player === "scissors" && computerChoice === "paper") // Scissors > Paper
+    ) {
+      console.log("Player Wins");
+    } 
+    // If not computer wins
+    else {
+      console.log("Computer Wins");
     }
-}
-
+  }
+  
